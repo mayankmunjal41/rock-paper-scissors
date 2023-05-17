77217cf026217267d2c1ec7ff437fc392b946ce8
Mayank Munjal <mayankmunjal95@gmail.com>
2023-05-18 05:06:45 +0530

    part 1 rps porject done

---
+++ index.js
@@ -0,0 +1,40 @@
+
+let randomText = ['rock', 'paper', 'scissors'];
+
+let getComputerChoice = () => {  
+    return randomText[Math.floor((Math.random() * randomText.length))];
+}
+let computerSelection = getComputerChoice();
+let playerSelection = prompt('Add your choice').toLowerCase();
+
+let playRound = (playerSelection, computerSelection) => {
+    if ( computerSelection === playerSelection) {
+        return 'Try again'
+    }  else if (computerSelection === 'rock' && playerSelection === 'paper' || computerSelection === 'paper' && playerSelection === 'scissors' || computerSelection === 'scissors' && playerSelection === 'rock' ) {
+        return 'Player wins'
+    } else {
+        return 'Computer wins'
+    }
+}
+console.log(playRound(playerSelection, computerSelection));
+
+
+
+
+let game = () => {
+    
+    // if playerchouce = computerchoice, try again
+    
+    // if playerchoice = rock && computerchoice = paper, computer wins
+   
+    // if playerchoice = paper && computerchoice = scissors, computer wins
+
+    // if playerchoice = scisors && computerchoice = rock, computer wins
+
+    // if playerchouce = rock && computerchoice = scissors, player wins
+
+    // if playerchoice = scissors && computerchoice = paper, player wins
+
+    // if playerchouce = paper && computerchoice = rock, player wins
+
+}
